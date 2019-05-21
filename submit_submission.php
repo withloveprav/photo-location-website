eb8e09f3e2585243d478cb1e535c2986a8471a88
withloveprav <32236182+withloveprav@users.noreply.github.com>
2019-05-21 14:23:54 -0400

    Create submit_submission.php
    This file contains the code to store the form data into a database as well as show the user that their submission was successful.

---
+++ submit_submission.php
@@ -0,0 +1,36 @@
+<html>
+
+<head>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+
+<body>
+<div class="whitebox">
+<?php
+  $first_name = $_POST["first_name"];
+  $last_name = $_POST["last_name"];
+  $email = $_POST["email"];
+  $instagram_handle = $_POST["instagram_handle"];
+  $post_url = $_POST["post_url"];
+
+  $con = mysqli_connect("localhost","pravallika","Murali12!", "photo_location_app");
+  if (!$con){
+    die('Could not connect: ' . mysqli_error($con));
+  }
+  
+  $sql= "INSERT INTO feature_submission (first_name, last_name, email, instagram_handle, post_url)
+  VALUES
+  ('$first_name', '$last_name', '$email', '$instagram_handle', '$post_url')";
+
+  if (!mysqli_query($con, $sql)){
+    die('Error: ' . mysqli_error($con));
+  }
+  echo "<h1>Thank you $first_name for your submission!</h1>";
+  echo "<p>We'll send an email to $email if $instagram_handle's photo is selected.</p>";
+
+  mysqli_close($con)
+?>
+</div>
+
+</body>
+</html>
